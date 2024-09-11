f3ef1ae5dc91013cd3008dd0f596fc55288ba782
Daniel <dcolinmorgan@gmail.com>
2024-09-11 15:31:24 +0800

    (slow) working flux multiCN-inpainting

--- LookBuilderPipeline/image_models/base_image_model.py
+++ LookBuilderPipeline/image_models/base_image_model.py
@@ -17,9 +17,36 @@ class BaseImageModel:
         self.mask = mask
         self.prompt = prompt
 
-    def generate_image(self):
+    def generate_image_extras(image):
         """
-        Method to be implemented by subclasses. 
-        Each model (SD3, Flux, etc.) will implement this method differently.
+        used to generate extra images for the various controlnets
         """
-        raise NotImplementedError("Subclasses must implement this method")
+        
+        from diffusers.utils import load_image
+        from controlnet_aux import HEDdetector, MidasDetector, MLSDdetector, OpenposeDetector, PidiNetDetector, NormalBaeDetector, LineartDetector, LineartAnimeDetector, CannyDetector, ContentShuffleDetector, ZoeDetector, MediapipeFaceDetector, SamDetector, LeresDetector, DWposeDetector
+        label=str(np.random.randint(100000000))
+        
+        #pose
+        openpose = OpenposeDetector.from_pretrained('lllyasviel/ControlNet')
+        pose_image = openpose(image)
+
+        #mask
+        segmenter = pipeline(model="mattmdjaga/segformer_b2_clothes")
+        segments = segmenter(image)
+        segment_include = ["Upper-clothes", "Skirt", "Pants", "Dress", "Belt", "Bag", "Scarf", "Right-shoe","Left-shoe","Bag"]
+        mask_list = [np.array(s['mask']) for s in segments if s['label'] not in segment_include]
+        final_mask = np.array(mask_list[0])
+        for mask in mask_list:
+            current_mask = np.array(mask)
+            final_mask = final_mask + current_mask  # Add the current mask to the final mask
+        final_array = final_mask.copy() 
+        final_mask = Image.fromarray(final_mask)
+
+        #canny (if needed)
+        canny = CannyDetector()
+        canny_image = canny(image)
+
+
+        showImagesHorizontally([image,final_mask,canny_image,pose_image],'input'+label+'.png')
+
+        return final_mask,canny_image,pose_image
--- LookBuilderPipeline/image_models/image_model_fl2.py
+++ LookBuilderPipeline/image_models/image_model_fl2.py
@@ -1,9 +1,35 @@
-from image_models.base_image_model import BaseImageModel
+import sys, os
+sys.path.insert(0, os.path.abspath('/flux-controlnet-inpaint/src'))
 import torch
-from diffusers import FluxControlNetPipeline, FluxControlNetModel
-from diffusers.models import FluxMultiControlNetModel
-from diffusers.utils import load_image
-# from PIL import Image
+from diffusers.pipelines.flux.pipeline_flux_controlnet_inpaint import FluxControlNetInpaintPipeline
+from diffusers.models.controlnet_flux import FluxControlNetModel
+from diffusers import FluxMultiControlNetModel
+from PIL import Image
+import numpy as np
+from transformers import SegformerImageProcessor, AutoModelForSemanticSegmentation
+import requests
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import os
+from pathlib import Path
+from controlnet_aux import CannyDetector
+from transformers import pipeline 
+
+from matplotlib.pyplot import figure, imshow, axis
+from matplotlib.image import imread
+
+
+def showImagesHorizontally(list_of_files, output_path='output.png'):
+    fig = figure()
+    number_of_files = len(list_of_files)
+    for i in range(number_of_files):
+        a=fig.add_subplot(1,number_of_files,i+1)
+        image = (list_of_files[i])
+        imshow(image,cmap='Greys_r')
+        axis('off')
+    plt.tight_layout()  # Adjust the layout to prevent overlapping
+    plt.savefig(output_path, dpi=300, bbox_inches='tight')  # Save the figure
+    plt.close(fig)  # Close the figure to free up memory
 
 class ImageModelFlux(BaseImageModel):
     def __init__(self, pose, mask, prompt):
@@ -14,35 +40,41 @@ class ImageModelFlux(BaseImageModel):
         Generate a new image using the Flux model based on the pose, mask and prompt.
         """
         # Set up the pipeline
-        base_model = 'black-forest-labs/FLUX.1-dev'
-        controlnet_model_union = 'InstantX/FLUX.1-dev-Controlnet-Union'
-
-        controlnet_union = FluxControlNetModel.from_pretrained(controlnet_model_union, torch_dtype=torch.float16, token=token)
-        controlnet = FluxMultiControlNetModel([controlnet_union])
-
-        pipe = FluxControlNetPipeline.from_pretrained(base_model, controlnet=controlnet, torch_dtype=torch.float16, token=token)
-        # pipe.to("cuda")
-        # Enable CPU offloading
-        # pipe.enable_model_cpu_offload()
-        pipe.enable_sequential_cpu_offload()
-        pipe.enable_xformers_memory_efficient_attention()
         
-        # prompt = 'A beautiful female model in paris.'
-        control_mode_depth = 2 # defines the depth of the controlnet
-        control_mode_pose = 4 # defines the pose of the controlnet
+        base_model = 'black-forest-labs/FLUX.1-dev'
+        # controlnet_model = 'YishaoAI/flux-dev-controlnet-canny-kid-clothes'
+        controlnet_model2 = 'InstantX/FLUX.1-dev-Controlnet-Union'
 
-        width, height = self.pose.size
+        # controlnet_canny = FluxControlNetModel.from_pretrained(controlnet_model, torch_dtype=torch.float16)
+        controlnet_pose = FluxControlNetModel.from_pretrained(controlnet_model2, torch_dtype=torch.float16)
+        controlnet = FluxMultiControlNetModel([controlnet_pose,controlnet_pose])
 
-        out_img = pipe(
-                self.prompt, 
-                control_image=[self.mask, self.pose],
-                control_mode=[control_mode_depth, control_mode_pose],
-                width=width,
-                height=height,
-                controlnet_conditioning_scale=[0.2, 0.4],
-                num_inference_steps=24, 
-                guidance_scale=3.5,
-                generator=torch.manual_seed(42),
-            ).images[0]
+        pipe = FluxControlNetInpaintPipeline.from_pretrained(base_model, controlnet=controlnet, torch_dtype=torch.float16)
+        pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+        # pipe.to("cuda")
+
+        pipe.text_encoder.to(torch.float16)
+        pipe.controlnet.to(torch.float16)
+        pipe.enable_sequential_cpu_offload()
+
+        images = [image, canny_image, pose_image, mask]
+        image, canny_image, pose_image, mask = [img.resize((512, 512)) for img in images]
+
+        generator = torch.Generator(device="cpu").manual_seed(seed)
+        image_res = pipe(
+            prompt,
+            image=image,
+            control_image=[canny_image,pose_image],
+            control_mode=[0, 4],
+            controlnet_conditioning_scale=[0.8,0.9],
+            mask_image=mask,
+            strength=0.95,
+            num_inference_steps=20,
+            guidance_scale=7.5,
+            generator=generator,
+            joint_attention_kwargs={"scale": scale},    
+        ).images[0]
+
+
 
         return out_img
